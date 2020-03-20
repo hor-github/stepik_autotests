e671453c0b7297a236e01d73996f9ad737a9e59c
hor <e.hoor@mail.ru>
2020-03-20 22:54:17 +0700

    Задание: поиск элемента по тексту в ссылке

--- test_stepik_1.py
+++ test_stepik_1.py
@@ -1,4 +1,3 @@
-
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.support.ui import Select
@@ -10,17 +9,19 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
-
-
-#driver = WebDriver(executable_path='C://selenium//chromedriver.exe')
+# driver = WebDriver(executable_path='C://selenium//chromedriver.exe')
 driver = WebDriver('/home/hor/ChromeDriver/chromedriver')
-#driver = WebDriver.Chrome(r'/ChromeDriver/chromedriver')
 
 
 def test_22 ():
-    driver.get('https://habr.com/ru/post/273897/')
-    time.sleep(10)
-    btn = driver.find_element(By.CSS_SELECTOR, "span.post__title-text")
+    try:
+        driver.get('http://suninjuly.github.io/simple_form_find_task.html')
+        input1 = driver.find_element(By.TAG_NAME, "input").send_keys("Ivan")
+        input2 = driver.find_element(By.NAME, "last_name").send_keys("Petrov")
+        input3 = driver.find_element(By.CLASS_NAME, "city").send_keys("Smolensk")
+        input4 = driver.find_element(By.ID, "country").send_keys("Russia")
+        button = driver.find_element(By.CSS_SELECTOR, ".btn").click()
 
-
-    driver.quit()
+    finally:
+        time.sleep(30)
+        driver.quit()
