03bb6afa38a166f79503897a71a871b7022e872b
hor <e.hoor@mail.ru>
2020-03-24 22:51:54 +0700

    Задание: поиск элемента по тексту в ссылке

--- test_stepik_1.py
+++ test_stepik_1.py
@@ -13,7 +13,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 driver = WebDriver('/home/hor/ChromeDriver/chromedriver')
 
 
-def test_22 ():
+def test_1 ():
     try:
         driver.get('http://suninjuly.github.io/simple_form_find_task.html')
         input1 = driver.find_element(By.TAG_NAME, "input").send_keys("Ivan")
@@ -24,4 +24,22 @@ def test_22 ():
 
     finally:
         time.sleep(30)
+        driver.quit()
+
+
+#  Search elements with help Selenium WebD
+def test_2 ():
+    try:
+        driver.get('http://suninjuly.github.io/find_link_text')
+        a = str(math.ceil(math.pow(math.pi, math.e) * 10000))
+        link1 = driver.find_element(By.PARTIAL_LINK_TEXT, "224592").click()
+
+        input1 = driver.find_element(By.TAG_NAME, "input").send_keys("Ivan")
+        input2 = driver.find_element(By.NAME, "last_name").send_keys("Petrov")
+        input3 = driver.find_element(By.CLASS_NAME, "city").send_keys("Smolensk")
+        input4 = driver.find_element(By.ID, "country").send_keys("Russia")
+        button = driver.find_element(By.CSS_SELECTOR, ".btn").click()
+
+    finally:
+        time.sleep(10)
         driver.quit()
