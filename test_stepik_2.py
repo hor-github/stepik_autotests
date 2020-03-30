e8aaa66523de1f598533a2e1662489fd4b09d812
hor <e.hoor@mail.ru>
2020-03-30 22:57:04 +0700

    checkbox and radiobutton

--- test_stepik_2.py
+++ test_stepik_2.py
@@ -1,4 +1,4 @@
-
+# -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.support.ui import Select
@@ -14,10 +14,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 driver = WebDriver('/home/hor/ChromeDriver/chromedriver')
 
 
+# основные методы Selenium
 
-
-
-
-
-12
 def test_1():
+    try:
+        def calc(x):
+            return str(math.log(abs(12 * math.sin(int(x)))))
+
+        driver.get("http://suninjuly.github.io/math.html")
+        x = driver.find_element(By.CSS_SELECTOR, "#input_value").text
+        y = calc(x)
+        input1 = driver.find_element(By.CSS_SELECTOR, ".form-group input").send_keys(y)
+        checkbox = driver.find_element(By.CSS_SELECTOR, ".form-check #robotCheckbox,#robotsRule").click()
+        # radio = driver.find_element(By.CSS_SELECTOR, "#robotsRule").click()
+        btnsubmit = driver.find_element(By.CSS_SELECTOR, ".btn").click()
+    finally:
+        time.sleep(30)
+        driver.quit()
